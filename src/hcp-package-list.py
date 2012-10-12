8b541a1c6833bd5b0d363137feae932d466a8e71
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-10-12 19:16:31 +0000

    Add the package-list script

---
+++ src/hcp-package-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import argparse
+
+from hippocp import Package
+
+parser = argparse.ArgumentParser(description='This is a script to list HCP packages.')
+parser.add_argument('-s', help='List system packages', action='store_true', default=True)
+parser.add_argument('-u', '--user', help='List packages for a specific user', required=False, default=None)
+args = parser.parse_args()
+
+pack = Package()
+print(pack.list(args.user, args.s))
+
