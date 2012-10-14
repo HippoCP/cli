5015805d9c18e93b0b2dc0785804c0ab9ef2203b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-10-14 15:24:16 +0000

    Add the user-add script

---
+++ src/hcp-user-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import argparse
+from hippocp import User
+
+parser = argparse.ArgumentParser(description='This is a script to add HCP users')
+parser.add_argument('-n', '--name', help='User name', required = True)
+parser.add_argument('-p', '--password', help='User password', required = True)
+parser.add_argument('-P', '--package', help='Package ID', required = True)
+parser.add_argument('-f', '--father', help='Father UID', required = True)
+args = parser.parse_args()
+
+u = User()
+print(u.add(args.name, args.package, args.father, args.password))
