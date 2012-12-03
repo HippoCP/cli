71833bf92b518a4d19e691d2eec9c3391aa91b8d
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-03 17:47:29 +0000

    Make the hcp-user-role functioning as expected

---
+++ src/hcp-user-role.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import argparse
+from hippocp.models import User
+
+parser = argparse.ArgumentParser(description='This is a script to manage HippoCP user roles.')
+parser.add_argument('-u', '--user', help='User id', required = True)
+parser.add_argument('-l', '--list', help='List roles', action='store_true')
+parser.add_argument('-c', '--check', help='Check specific role', action='store_true')
+parser.add_argument('-r', '--role', help='Role (required for check)')
+args = parser.parse_args()
+
+if args.list is True:
+	u = User(args.user)
+	print(u.roles())
+
+if args.check is True:
+	u = User(args.user)
+	print(u.checkRole(args.role))
