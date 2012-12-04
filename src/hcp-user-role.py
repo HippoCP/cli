05108e3287cb06af4c560f94a60f6ee6666e3d44
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-04 12:26:31 +0000

    Implement the add-role function

--- src/hcp-user-role.py
+++ src/hcp-user-role.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='This is a script to manage HippoCP
 parser.add_argument('-u', '--user', help='User id', required = True)
 parser.add_argument('-l', '--list', help='List roles', action='store_true')
 parser.add_argument('-c', '--check', help='Check specific role', action='store_true')
+parser.add_argument('-a', '--add-role', help='Add role to the user', action='store_true')
 parser.add_argument('-r', '--role', help='Role (required for check)')
 args = parser.parse_args()
 
@@ -17,3 +18,7 @@ if args.list is True:
 if args.check is True:
 	u = User(args.user)
 	print(u.checkRole(args.role))
+
+if args.add_role is True:
+	u = User(args.user)
+	print(u.addRole(args.role))
