b5899ec9b24e9d10d9caf2c801a9047dd24060cf
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-16 15:36:23 +0000

    Add add-role capability to hcp-roles

--- src/hcp-roles.py
+++ src/hcp-roles.py
@@ -5,8 +5,14 @@ from hippocp import Roles
 
 parser = argparse.ArgumentParser(description='This is a script to manage HippoCP user roles.')
 parser.add_argument('-l', '--list', help='List roles', action='store_true')
+parser.add_argument('-a', '--add', help='Add role', action='store_true')
+parser.add_argument('-n', '--name', help='Role (required for add)')
 args = parser.parse_args()
 
 if args.list is True:
 	r = Roles()
 	print(r.list())
+
+if args.add is True:
+	r = Roles()
+	print(r.add(args.name))
