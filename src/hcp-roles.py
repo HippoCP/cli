5b11f7263db3b7be4384a2c2613eb560750891e1
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-17 23:04:15 +0000

    Add remove-role capability to hcp-roles

--- src/hcp-roles.py
+++ src/hcp-roles.py
@@ -6,6 +6,8 @@ from hippocp import Roles
 parser = argparse.ArgumentParser(description='This is a script to manage HippoCP user roles.')
 parser.add_argument('-l', '--list', help='List roles', action='store_true')
 parser.add_argument('-a', '--add', help='Add role', action='store_true')
+parser.add_argument('-r', '--remove', help='Remove role', action='store_true')
+parser.add_argument('-i', '--id', help='Role (required for remove)')
 parser.add_argument('-n', '--name', help='Role (required for add)')
 args = parser.parse_args()
 
@@ -16,3 +18,7 @@ if args.list is True:
 if args.add is True:
 	r = Roles()
 	print(r.add(args.name))
+
+if args.remove is True:
+	r = Roles()
+	print(r.remove(args.id))
