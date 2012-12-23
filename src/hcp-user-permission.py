de193538754c943791c37bebbdea300a33783a60
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-23 17:09:32 +0000

    Add to hcp-user-permission the check capability

--- src/hcp-user-permission.py
+++ src/hcp-user-permission.py
@@ -6,9 +6,14 @@ from hippocp.models import User
 parser = argparse.ArgumentParser(description='This is a script to manage HippoCP user permissions.')
 parser.add_argument('-u', '--user', help='User id', required = True)
 parser.add_argument('-l', '--list', help='List permissions', action='store_true')
+parser.add_argument('-c', '--check', help='Check specific permission', action='store_true')
+parser.add_argument('-r', '--permission', help='Permission (required for check)')
 args = parser.parse_args()
 
 if args.list is True:
 	u = User(args.user)
 	print(u.permissions())
-	
+
+if args.check is True:
+	u = User(args.user)
+	print(u.checkPermission(args.permission))
