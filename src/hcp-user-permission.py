a5d01113453e47ecaf5c52e330976a6f9335fd1b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-25 21:34:05 +0000

    Add to hcp-user-permission the remove capability

--- src/hcp-user-permission.py
+++ src/hcp-user-permission.py
@@ -8,7 +8,8 @@ parser.add_argument('-u', '--user', help='User id', required = True)
 parser.add_argument('-l', '--list', help='List permissions', action='store_true')
 parser.add_argument('-c', '--check', help='Check specific permission', action='store_true')
 parser.add_argument('-a', '--add-permission', help='Add permission to the user', action='store_true')
-parser.add_argument('-r', '--permission', help='Permission (required for check, add)')
+parser.add_argument('-d', '--remove-permission', help='Remove permission to the user', action='store_true')
+parser.add_argument('-r', '--permission', help='Permission (required for check, add, remove)')
 args = parser.parse_args()
 
 if args.list is True:
@@ -22,3 +23,7 @@ if args.check is True:
 if args.add_permission is True:
 	u = User(args.user)
 	print(u.addPermission(args.permission))
+
+if args.remove_permission is True:
+	u = User(args.user)
+	print(u.removePermission(args.permission))
